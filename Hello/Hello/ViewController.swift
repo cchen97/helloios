a343241febc0c4a907e8944b825d47eb3801d3a5
cchen97 <manjic@uw.edu>
2019-01-10 17:08:18 -0800

    almost

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Charlene on 1/10/19.
+//  Copyright © 2019 Charlene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
